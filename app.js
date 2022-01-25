7e4abbf1c8f0966e262bb7609cb0fa66b0442baa
Chance <chancecrawford30@gmail.com>
2022-01-24 17:54:00 -0800

    user can add a department to the database

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const { viewDepartments } = require('./utils/sqlQueries');
+const { viewDepartments, viewRoles, viewEmployees, addDepartment } = require('./utils/sqlQueries');
 
 function promptAction(){
     // After user chooses their desired option from the list.
@@ -18,15 +18,53 @@ function promptAction(){
 
 function performAction(choiceObj){
     // destructure object for choice property
-    const { choice } = choiceObj
+    const { choice } = choiceObj;
+
+    if(choice === 'QUIT'){
+        // ends application
+        process.exit()
+    }
 
     if(choice === 'View all departments'){
+        // shows that department data in a table and then
+        // prompts the user to choose a different action.
         return viewDepartments().then(promptAction);
     }
 
-    if(choice === 'QUIT'){
-        process.exit()
+    if(choice === 'View all roles'){
+        // shows that department data in a table and then
+        // prompts the user to choose a different action.
+        return viewRoles().then(promptAction);
     }
+
+    if(choice === 'View all employees'){
+        return viewEmployees().then(promptAction);
+    }
+
+    if(choice === 'Add a department'){
+
+        // prompts for name, then adds department, then 
+        // asks user if there is another action they would
+        // like to take.
+        return inquirer.prompt({
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?',
+            validate: nameInput =>{
+                if(nameInput){
+                return true;
+                }
+                else{
+                console.log("Please enter the department's name");
+                return false;
+                }
+            }
+        })
+        .then(addDepartment)
+        .then(promptAction);
+    }
+
+    
 }
 
 // events
--- utils/sqlQueries.js
+++ utils/sqlQueries.js
@@ -1,8 +1,8 @@
 // npm package that displays data in a better formatted table
 const cTable = require('console.table');
 // connects to the MySQL database but this is the promised based
-// package. so db.query will return a promise that can have a "then"
-// function run after it
+// package. so db.query will return a promise that can have a "spread"
+// or a "then" function run after it.
 var db = require('mysql2-promise')();
 
 db.configure({
@@ -15,11 +15,56 @@ db.configure({
 function viewDepartments(){
     const sql = `SELECT * FROM department`;
 
+    // made the MySQL query promise based so that we could run
+    // a "then" function after this viewDepartments()
+    // function returns.
     return db.query(sql).spread(rows =>{
         console.table(rows);
     });
 }
 
+function viewRoles(){
+    const sql = `SELECT * FROM role`;
+
+    // made the MySQL query promise based so that we could run
+    // a "then" function after this viewDepartments()
+    // function returns.
+    return db.query(sql).spread(rows =>{
+        console.table(rows);
+    });
+}
+
+function viewEmployees(){
+    const sql = `SELECT * FROM employee`;
+
+    return db.query(sql).spread(rows =>{
+        console.table(rows);
+    });
+}
+
+function addDepartment(departmentObj){
+
+    const { departmentName } = departmentObj;
+
+    // insert new department into the database
+    const sql = `INSERT INTO department(name)
+                 VALUES (?)
+    `;
+    const params = [departmentName]
+
+    return db.query(sql, params).spread((rows, err) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log('Department Added!');
+        }  
+    });
+}
+
 module.exports = {
-    viewDepartments
+    viewDepartments, 
+    viewRoles,
+    viewEmployees,
+    addDepartment
 }
