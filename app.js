01495a89e3a17cdd21b926cdcaa8f00051a0c66f
Chance <chancecrawford30@gmail.com>
2022-01-25 16:23:08 -0800

    user can update employee roles

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole } = require('./utils/sqlQueries');
+const { viewDepartments, viewRoles, getEmployeeObjects, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } = require('./utils/sqlQueries');
 
 function promptAction(){
     // After user chooses their desired option from the list.
@@ -41,6 +41,7 @@ function performAction(choiceObj){
         return viewEmployees().then(promptAction);
     }
 
+    // conditional for adding a department
     if(choice === 'Add a department'){
 
         // prompts for name, then adds department, then 
@@ -64,6 +65,7 @@ function performAction(choiceObj){
         .then(promptAction);
     }
 
+    // conditional for adding a role
     if(choice === 'Add a role'){
 
         // prompts for info, then adds role to database, then 
@@ -114,6 +116,9 @@ function performAction(choiceObj){
             }
         ])
         .then(addRole)
+        // based on whether a row was added
+        // to the database, return a success or
+        // error message
         .then((queryInfo)=>{
             if(queryInfo[0].affectedRows === 1){
                 console.log('Role added to database');
@@ -123,6 +128,123 @@ function performAction(choiceObj){
             }
         })
         .then(promptAction);
+    }
+
+    // conditional for adding an employee
+    if(choice === 'Add an employee'){
+
+        // prompts for info, then adds employee to database, then 
+        // asks user if there is another action they would
+        // like to take.
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: "What is the employee's first name?",
+                validate: input =>{
+                    if(input){
+                    return true;
+                    }
+                    else{
+                    console.log("Please enter the employee's first name");
+                    return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "What is the employee's last name?",
+                validate: input =>{
+                    if(input){
+                    return true;
+                    }
+                    else{
+                    console.log("Please enter the employee's last name");
+                    return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'employeeRole',
+                message: "What is the employee's role?",
+                validate: input =>{
+                    if(input){
+                    return true;
+                    }
+                    else{
+                    console.log("Please enter the employee's role");
+                    return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: "Please enter the ID of the employee's manager. (Press enter if the employee does not have a manager)"
+            }
+        ])
+        .then(addEmployee)
+        // based on whether a row was added
+        // to the database, return a success or
+        // error message
+        .then((queryInfo)=>{
+            if(queryInfo[0].affectedRows === 1){
+                console.log('Employee added to database');
+            }
+            else{
+                console.log('Failed to add employee to database, please try again');
+            }
+        })
+        .then(promptAction);
+    }
+
+    if(choice === 'Update an employee role'){
+
+        // returns an array of the employees from the database
+        // so the array can then be added to the choices field
+        // of the inquirer.prompt list
+        getEmployeeObjects()
+        .then(arr =>{
+            return inquirer.prompt([
+                {
+                    type: 'list',
+                    pageSize: 8,
+                    message: 'Which employee role do you want to update:',
+                    name: 'employeeName',
+                    choices: arr
+                },
+                {
+                    type: 'input',
+                    name: 'newRole',
+                    message: "What is the employee's new role?",
+                    validate: input =>{
+                        if(input){
+                        return true;
+                        }
+                        else{
+                        console.log("Please enter the employee's new role");
+                        return false;
+                        }
+                    }
+                }
+            ]);
+        })
+        .then(updateEmployeeRole)
+        // based on whether a row was updated
+        // in the database, return a success or
+        // error message
+        .then((queryInfo)=>{
+            if(queryInfo[0].affectedRows === 1){
+                console.log('Employee role updated');
+            }
+            else{
+                console.log('Failed to update employee role, please try again');
+            }
+        })
+        .then(promptAction);
+        
     }
 
     
--- utils/sqlQueries.js
+++ utils/sqlQueries.js
@@ -24,7 +24,11 @@ function viewDepartments(){
 }
 
 function viewRoles(){
-    const sql = `SELECT * FROM role`;
+    const sql = `SELECT role.*, department.name AS department
+                 FROM role
+                 LEFT JOIN department ON role.department_id = department.id
+                 
+    `;
 
     // made the MySQL query promise based so that we could run
     // a "then" function after this viewDepartments()
@@ -35,10 +39,58 @@ function viewRoles(){
 }
 
 function viewEmployees(){
-    const sql = `SELECT * FROM employee`;
+
+    // select all columns from employee table except for role_id column,
+    // select the title and salary columns from the role table
+    // (and give them aliases in the new table we are displaying),
+    // then select the name column from the department table.
+
+    // left join will join all specified columns onto the table
+    // specified in the FROM command.
+
+    // left join role will match the employee.role_id column
+    // to the id of the role in the role table and merge them
+    // together. Saying left join role only joins the columns
+    // specified in the role select statement, in this case
+    // title and salary from the role table will be joined.
+
+    // we want to join the department.name column when we match
+    // the role_id to the employee role id. Since the role
+    // table contains the foreign key to determine which department
+    // the role belongs to (the department_id column in the role table)
+    // we need to match up the role id to the employees role id first
+    // and then select that same role, and get its department_id.
+    // that way we can be sure the employee's role is being matched to
+    // the correct department.
+    const sql = `
+                 SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, role.title AS role, role.salary AS salary, department.name AS employee_department
+                 FROM employee 
+                 LEFT JOIN role ON employee.role_id = role.id      
+                 LEFT JOIN department ON employee.role_id = role.id WHERE role.department_id = department.id     
+    `;
 
     return db.query(sql).spread(rows =>{
         console.table(rows);
+    });
+}
+
+// returns an array of the employees from the database
+// so the array can then be added to the choices field
+// of the inquirer.prompt list
+function getEmployeeObjects(){
+    let employeeArr = [];
+    const sql = `SELECT * FROM employee`;
+
+    return db.query(sql).spread(rows =>{
+        for(let i = 0; i < rows.length; i++){
+            let name;
+            
+            name = `${rows[i].first_name} ${rows[i].last_name}`;
+
+            employeeArr.push(name);
+        }
+
+        return employeeArr;
     });
 }
 
@@ -65,7 +117,7 @@ function addDepartment(departmentObj){
     });
 }
 
-async function addRole(roleObj){
+function addRole(roleObj){
 
     // expecting an object from inquirer.prompt
     // takes object and destructures it to get
@@ -86,7 +138,7 @@ async function addRole(roleObj){
 
         return db.query(departmentSql, departmentParam).spread(row =>{
             // finds the id of the name that the user entered
-            // and the extracts the id value from the object that
+            // and then extracts the id value from the object that
             // was returned from the database.
             // then assigns it to department_id variable
             department_id = row[0].id;
@@ -114,10 +166,106 @@ async function addRole(roleObj){
     }) 
 }
 
+function addEmployee(employeeObj){
+
+    // expecting an object from inquirer.prompt
+    // takes object and destructures it to get
+    // the info to put in the database.
+    const { firstName, lastName, employeeRole, manager } = employeeObj;
+    
+    let role_id;
+
+    // turn into a number
+    let manager_id = Number(manager);
+
+    // if user didnt assign a manager, then 0 will be returned
+    // by inquirer once manager_id tried to parse it to a number.
+    // so if the variable is less than one, just set it to null.
+    if(manager_id < 1){
+        manager_id = null;
+    }
+
+    // created a function within a function so that the 
+    // id number can be returned from the async database 
+    // call. and then a "then" statement can be used below
+    // to pass the id into the other database call successfully.
+    function getEmployeeId(){
+        // get id of role based on title from user
+        const employeeSql = `SELECT id FROM role WHERE title= (?)`;
+        const employeeParam = [employeeRole];
+
+
+        return db.query(employeeSql, employeeParam).spread(row =>{
+            // finds the id of the role that the user entered
+            // and then extracts the id value from the object that
+            // was returned from the database.
+            // then assigns it to role_id variable
+            role_id = row[0].id;
+        });
+    }
+    
+    // get the id, function above, then insert the employee with the 
+    // role_id into the database
+    return getEmployeeId()
+    .then(()=>{
+        // insert new employee into the database
+        const sql = `INSERT INTO employee(first_name, last_name, role_id, manager_id)
+                     VALUES (?, ?, ?, ?)
+        `;
+        // these are the names I gave to the properties of the
+        // inquirer object.
+        const params = [firstName, lastName, role_id, manager_id];
+
+        // return the query response info to check the affectedRows
+        // in a "then" statement in app.js.
+        // this is done so that the "role added" message
+        // does not appear after the new selection menu
+        // and is synchronous
+        return db.query(sql, params);
+    }) 
+}
+
+function updateEmployeeRole(roleObj){
+    const { employeeName, newRole } = roleObj;
+
+    const employeeFirst = employeeName.split(' ')[0];
+    const employeeLast = employeeName.split(' ')[1];
+
+    let role_id;
+
+    function getRoleId(){
+        // get id of new role based on title from user
+        const roleSql = `SELECT id FROM role WHERE title= (?)`;
+        const roleParam = [newRole];
+
+
+        return db.query(roleSql, roleParam).spread(row =>{
+            // finds the id of the role that the user entered
+            // and then extracts the id value from the object that
+            // was returned from the database.
+            // then assigns it to role_id variable
+            role_id = row[0].id;
+        });
+    }
+
+    return getRoleId()
+    .then(() =>{
+        const sql = `UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?`;
+        const params = [role_id, employeeFirst, employeeLast]
+
+        return db.query(sql, params)
+    });
+    
+}
+
+
 module.exports = {
     viewDepartments, 
     viewRoles,
+    getEmployeeObjects,
     viewEmployees,
     addDepartment, 
-    addRole
+    addRole,
+    addEmployee,
+    updateEmployeeRole
 }
